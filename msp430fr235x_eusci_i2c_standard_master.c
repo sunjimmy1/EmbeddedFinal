343b1beb428e8066586ea1b866d34cd35d73b9ad
Jimmy Sunbury <jimmysunbury@gmail.com>
2022-12-15 16:00:30 -0500

    More semi working code

--- msp430fr235x_eusci_i2c_standard_master.c
+++ msp430fr235x_eusci_i2c_standard_master.c
@@ -89,7 +89,7 @@ unsigned long RawHumidity = 0;
 float Humidity = 0;
 char tempStr[16] = { "                " };
 char humStr[16] = { "                " };
-;
+
 //******************************************************************************
 // General I2C State Machine ***************************************************
 //******************************************************************************
@@ -162,6 +162,143 @@ void data(uint8_t data);
 void blocks();
 void initOled();
 void output(char *line1, char *line2);
+
+void uart_init(void);
+void ConfigClocks(void);
+void strreverse(char *begin, char *end);
+void itoa(int value, char *str, int base);
+void Software_Trim();
+void port_init();
+
+void uart_init(void)
+{
+    UCA1CTLW0 |= UCSWRST;
+    UCA1CTLW0 |= UCSSEL__SMCLK;
+    UCA1BRW = 8;                             // 115200
+    UCA1MCTLW = 0xD600;
+    UCA1CTLW0 &= ~UCSWRST;                    // Initialize eUSCI
+    UCA1IE |= UCRXIE;                         // Enable USCI_A0 RX interrupt
+}
+void ConfigClocks(void)
+{
+    CSCTL3 = SELREF__REFOCLK;               // Set REFO as FLL reference source
+    CSCTL1 = DCOFTRIMEN_1 | DCOFTRIM0 | DCOFTRIM1 | DCORSEL_0; // DCOFTRIM=3, DCORange = 1MHz
+    CSCTL2 = FLLD_0 + 30;                   // DCODIV = 1MHz
+    __delay_cycles(3);
+    __bic_SR_register(SCG0);                // Enable FLL
+    Software_Trim();             // Software Trim to get the best DCOFTRIM value
+    CSCTL4 = SELMS__DCOCLKDIV | SELA__REFOCLK; // set default REFO(~32768Hz) as ACLKsource, ACLK = 32768Hz
+    // default DCODIV as MCLK and SMCLK source
+}
+void strreverse(char *begin, char *end)
+// Function to reverse the order of the ASCII char array elements
+{
+    char aux;
+    while (end > begin)
+        aux = *end, *end-- = *begin, *begin++ = aux;
+}
+void itoa(int value, char *str, int base)
+{  //Function to convert the signed int to an ASCII char array
+    static char num[] = "0123456789abcdefghijklmnopqrstuvwxyz";
+    char *wstr = str;
+    int sign;
+    // Validate that base is between 2 and 35 (inlcusive)
+    if (base < 2 || base > 35)
+    {
+        *wstr = '\0';
+        return;
+    }
+    // Get magnitude and th value
+    sign = value;
+    if (sign < 0)
+        value = -value;
+    do // Perform interger-to-string conversion.
+        *wstr++ = num[value % base]; //create the next number in converse by taking the modolus
+    while (value /= base);  // stop when you get  a 0 for the quotient
+    if (sign < 0) //attch sign character, if needed
+        *wstr++ = '-';
+    *wstr = '\0'; //Attach a null character at end of char array. The string is in revers order at this point
+    strreverse(str, wstr - 1); // Reverse string
+}
+void port_init()
+{
+    P1DIR = BIT6; // P1.6 outputs
+    P1OUT = 0; // LEDs off
+
+    P1DIR |= BIT0;
+    ConfigClocks();
+    port_init();
+    uart_init();
+    P1OUT |= BIT0;
+    P1SEL0 |= BIT6 | BIT7;                  // set 2-UART pin as second function
+    P4SEL0 |= BIT2 | BIT3;                  // set 2-UART pin as second function
+    P4SEL1 &= ~BIT2;                    // set 2-UART pin as second function
+    P4SEL1 &= ~ BIT3;                    // set 2-UART pin as second function
+}
+void Software_Trim()
+{
+    unsigned int oldDcoTap = 0xffff;
+    unsigned int newDcoTap = 0xffff;
+    unsigned int newDcoDelta = 0xffff;
+    unsigned int bestDcoDelta = 0xffff;
+    unsigned int csCtl0Copy = 0;
+    unsigned int csCtl1Copy = 0;
+    unsigned int csCtl0Read = 0;
+    unsigned int csCtl1Read = 0;
+    unsigned int dcoFreqTrim = 3;
+    unsigned char endLoop = 0;
+    do
+    {
+        CSCTL0 = 0x100;                         // DCO Tap = 256
+        do
+        {
+            CSCTL7 &= ~DCOFFG;                  // Clear DCO fault flag
+        }
+        while (CSCTL7 & DCOFFG);               // Test DCO fault flag
+        //__delay_cycles((unsigned int)3000 * MCLK_FREQ_MHZ);// Wait FLL lock status (FLLUNLOCK) tobe stable
+        // Suggest to wait 24 cycles of divided FLL reference clock
+        while ((CSCTL7 & (FLLUNLOCK0 | FLLUNLOCK1)) && ((CSCTL7 & DCOFFG) == 0))
+            ;
+        csCtl0Read = CSCTL0;                   // Read CSCTL0
+        csCtl1Read = CSCTL1;                   // Read CSCTL1
+        oldDcoTap = newDcoTap;               // Record DCOTAP value of last time
+        newDcoTap = csCtl0Read & 0x01ff;       // Get DCOTAP value of this time
+        dcoFreqTrim = (csCtl1Read & 0x0070) >> 4;       // Get DCOFTRIM value
+        if (newDcoTap < 256)                    // DCOTAP < 256
+        {
+            newDcoDelta = 256 - newDcoTap; // Delta value between DCPTAP and 256
+            if ((oldDcoTap != 0xffff) && (oldDcoTap >= 256)) // DCOTAP cross 256
+                endLoop = 1;                   // Stop while loop
+            else
+            {
+                dcoFreqTrim--;
+                CSCTL1 = (csCtl1Read & (~DCOFTRIM)) | (dcoFreqTrim << 4);
+            }
+        }
+        else                                   // DCOTAP >= 256
+        {
+            newDcoDelta = newDcoTap - 256; // Delta value between DCPTAP and 256
+            if (oldDcoTap < 256)                // DCOTAP cross 256
+                endLoop = 1;                   // Stop while loop
+            else
+            {
+                dcoFreqTrim++;
+                CSCTL1 = (csCtl1Read & (~DCOFTRIM)) | (dcoFreqTrim << 4);
+            }
+        }
+        if (newDcoDelta < bestDcoDelta)         // Record DCOTAP closest to 256
+        {
+            csCtl0Copy = csCtl0Read;
+            csCtl1Copy = csCtl1Read;
+            bestDcoDelta = newDcoDelta;
+        }
+    }
+    while (endLoop == 0);                      // Poll until endLoop == 1
+    CSCTL0 = csCtl0Copy;                       // Reload locked DCOTAP
+    CSCTL1 = csCtl1Copy;                       // Reload locked DCOFTRIM
+    while (CSCTL7 & (FLLUNLOCK0 | FLLUNLOCK1))
+        ; // Poll until FLL is locked
+}
 
 void output(char *line1, char *line2)
 {
@@ -367,33 +504,36 @@ int main(void)
     initClockTo16MHz();
     initGPIO();
     initI2C();
-
     initOled();
+//    ConfigClocks();
+//    port_init();
+//    uart_init();
+
     blocks();
     while (1)
     {
-        I2C_Master_ReadReg(SLAVE_ADDR, 0x00, 3);
-        __no_operation();
-        __delay_cycles(30000);
-        I2C_Master_ReadReg(SLAVE_ADDR, 0x71, 1);
-        I2C_Master_WriteReg(SLAVE_ADDR, 0xAC, MasterType2, 2);
-        __delay_cycles(30000);
-        I2C_Master_ReadReg(SLAVE_ADDR, 0x71, 6);
-        CopyArray(ReceiveBuffer, HumidityData, HDataLength, 1);
-        CopyArray(ReceiveBuffer, TempData, TDataLength, 3);
-        RawHumidity = (HumidityData[2] >> 4) | (HumidityData[1] << 4)
-                | ((long) HumidityData[0] << 12);
-        Humidity = (float) (RawHumidity >> 4);
-        Humidity = (Humidity / 65536) * 100;
-
-        RawTemp = ((long) (TempData[0] & 0x0F) << 16)
-                | ((long) TempData[1] << 8) | TempData[2];
-        Temp = (float) (RawTemp >> 4);
-        Temp = ((((Temp / 65536) * 200 - 50) * 9) / 5) + 32;
-        sprintf(humStr, "Humidity: %2.2f%%", Humidity);
-        sprintf(tempStr, "Temp: %2.2fF    ", Temp);
-        output(humStr, tempStr);
-        __no_operation();
+//        I2C_Master_ReadReg(SLAVE_ADDR, 0x00, 3);
+//        __no_operation();
+//        __delay_cycles(30000);
+//        I2C_Master_ReadReg(SLAVE_ADDR, 0x71, 1);
+//        I2C_Master_WriteReg(SLAVE_ADDR, 0xAC, MasterType2, 2);
+//        __delay_cycles(30000);
+//        I2C_Master_ReadReg(SLAVE_ADDR, 0x71, 6);
+//        CopyArray(ReceiveBuffer, HumidityData, HDataLength, 1);
+//        CopyArray(ReceiveBuffer, TempData, TDataLength, 3);
+//        RawHumidity = (HumidityData[2] >> 4) | (HumidityData[1] << 4)
+//                | ((long) HumidityData[0] << 12);
+//        Humidity = (float) (RawHumidity >> 4);
+//        Humidity = (Humidity / 65536) * 100;
+//
+//        RawTemp = ((long) (TempData[0] & 0x0F) << 16)
+//                | ((long) TempData[1] << 8) | TempData[2];
+//        Temp = (float) (RawTemp >> 4);
+//        Temp = ((((Temp / 65536) * 200 - 50) * 9) / 5) + 32;
+//        sprintf(humStr, "Humidity: %2.2f%%", Humidity);
+//        sprintf(tempStr, "Temp: %2.2fF    ", Temp);
+//        output(humStr, tempStr);
+//        __no_operation();
         __delay_cycles(5000000);
     }
 }
